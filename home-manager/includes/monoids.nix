a13dd3f373557140a4a0a5d5239112083ec75edb
wchresta <34962284+wchresta@users.noreply.github.com>
2022-09-06 23:06:56 +0200

    Add monoid@netmonoid

--- home-manager/includes/monoids.nix
+++ home-manager/includes/monoids.nix
@@ -4,7 +4,15 @@
   config = {
     programs.ssh.matchBlocks = {
       "netmonoid" = {
+        user = "monoid";
+        host = "netmonoid";
+        port = 144;
+        identityFile = "~/.ssh/id_monoids_monoid";
+      };
+
+      "netmonoid-deploy" = {
         user = "root";
+        host = "netmonoid";
         port = 144;
         identityFile = "~/.ssh/id_monoids";
       };
