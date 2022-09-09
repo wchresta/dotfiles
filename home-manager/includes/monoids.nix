ec2e522d5d00de96bfe9351b2f098f3f58bc9d53
wchresta <34962284+wchresta@users.noreply.github.com>
2022-09-09 20:49:31 +0200

    Move monoids keyfiles

--- home-manager/includes/monoids.nix
+++ home-manager/includes/monoids.nix
@@ -7,21 +7,21 @@
         user = "monoid";
         host = "netmonoid";
         port = 144;
-        identityFile = "~/.ssh/id_monoids_monoid";
+        identityFile = "~/.ssh/monoids/id_monoids_monoid";
       };
 
       "netmonoid-deploy" = {
         user = "root";
         host = "netmonoid";
         port = 144;
-        identityFile = "~/.ssh/id_monoids";
+        identityFile = "~/.ssh/monoids/id_monoids";
       };
 
       "netmonoid-config" = {
         user = "config";
         host = "netmonoid";
         port = 144;
-        identityFile = "~/.ssh/id_monoids_config";
+        identityFile = "~/.ssh/monoids/id_monoids_config";
       };
     };
   };
