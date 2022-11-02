e6c9c1c54f650d19853d4821fa1dcc3bd2778a88
wchresta <34962284+wchresta@users.noreply.github.com>
2022-11-02 17:50:26 +0100

    Switch back to public based IP

--- home-manager/includes/monoids.nix
+++ home-manager/includes/monoids.nix
@@ -6,21 +6,21 @@
     programs.ssh.matchBlocks = {
       "netmonoid" = {
         user = "monoid";
-        host = "net.monoid.li";
+        host = "netmonoid";
         port = 144;
         identityFile = "~/.ssh/monoids/id_monoids_monoid";
       };
 
       "netmonoid-deploy" = {
         user = "root";
-        host = "net.monoid.li";
+        host = "netmonoid";
         port = 144;
         identityFile = "~/.ssh/monoids/id_monoids";
       };
 
       "netmonoid-config" = {
         user = "git-config";
-        host = "net.monoid.li";
+        host = "netmonoid";
         port = 144;
         identityFile = "~/.ssh/monoids/id_monoids_config";
       };
