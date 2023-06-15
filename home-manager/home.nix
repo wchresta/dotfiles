4c947b26d1b02fd7c5f0bb671095544cef0a31c6
wchresta <34962284+wchresta@users.noreply.github.com>
2023-06-15 21:05:50 +0200

    Add fzf

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -112,6 +112,26 @@ in {
     };
   };
 
+  programs.fzf = {
+    enable = true;
+    # Gruvbox dark
+    colors = {
+      "fg" = "#ebdbb2";
+      "bg" = "#282828";
+      "hl" = "#fabd2f";
+      "fg+" = "#ebdbb2";
+      "bg+" = "#3c3836";
+      "hl+" = "#fabd2f";
+
+      "info" = "#83a598";
+      "prompt" = "#bdae93";
+      "spinner" = "#fabd2f";
+      "pointer" = "#83a598";
+      "marker" = "#fe8019";
+      "header" = "#665c54";
+    };
+  };
+
   programs.zsh = {
     inherit shellAliases;
     enable = true;
