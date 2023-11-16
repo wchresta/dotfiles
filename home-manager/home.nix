986bb3c3abcfc05bcb5411f655871c57741dfdb0
wchresta <34962284+wchresta@users.noreply.github.com>
2023-11-16 17:46:25 +0100

    Move everything shell into its own module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,38 +1,6 @@
 { config, lib, pkgs, ... }:
 
-let
-  localLib = pkgs.callPackage ./lib.nix {};
-
-  shellAliases = let
-      cfg-src = "~/src/dotfiles/home-manager";
-      ehome-cmd = file: with pkgs; ''
-        git -C ${cfg-src} pull && \
-        nvim ${cfg-src}/${file} && \
-        git -C ${cfg-src}/${file} add ${cfg-src}/${file} && \
-        sudo nixos-rebuild switch --update-input monoid-home --update-input light-control && \
-        git -C ${cfg-src} commit
-      '';
-    in {
-      ls = "ls --color=auto";
-      l = "ls -alh";
-      ll = "ls -lh";
-      la = "ls -a";
-
-      # Ensure ssh'ed hosts understand kitty
-      ssh = "kitty +kitten ssh";
-
-      ehome = ehome-cmd "";
-      evim = ehome-cmd "includes/vim.nix";
-
-      enix = ''
-        sudo -E nvim /etc/nixos/ && \
-          sudo -E ${pkgs.git}/bin/git -C /etc/nixos add /etc/nixos && \
-          sudo /run/current-system/sw/bin/nixos-rebuild switch && \
-          sudo -E ${pkgs.git}/bin/git -C /etc/nixos commit
-      '';
-    };
-
-in {
+{
   imports =
     [
       includes/browser.nix
@@ -41,6 +9,7 @@ in {
       includes/kitty.nix
       includes/latex.nix
       includes/monoids.nix
+      includes/shell.nix
       includes/vim.nix
       includes/windowManager.nix
     ];
@@ -98,56 +67,6 @@ in {
   };
 
   programs.chromium = {
-    enable = true;
-  };
-
-  programs.powerline-go = {
-    enable = true;
-    modules = [ "host" "ssh" "cwd" "nix-shell" "gitlite" "exit" ];
-    # Soon this is supported:
-    # modulesRight = [ "git" "goenv" "venv" ];
-    settings = {
-      hostname-only-if-ssh = true;
-      cwd-max-depth = 4;
-      theme = "gruvbox";
-      numeric-exit-codes = true;
-    };
-  };
-
-  programs.fzf = {
-    enable = true;
-    # Gruvbox dark
-    colors = {
-      "fg" = "#ebdbb2";
-      "bg" = "#282828";
-      "hl" = "#fabd2f";
-      "fg+" = "#ebdbb2";
-      "bg+" = "#3c3836";
-      "hl+" = "#fabd2f";
-
-      "info" = "#83a598";
-      "prompt" = "#bdae93";
-      "spinner" = "#fabd2f";
-      "pointer" = "#83a598";
-      "marker" = "#fe8019";
-      "header" = "#665c54";
-    };
-  };
-
-  programs.zsh = {
-    inherit shellAliases;
-    enable = true;
-    enableSyntaxHighlighting = true;
-
-    defaultKeymap = "viins";
-    initExtra = ''
-      # Enable shift-tab reverse completion
-      bindkey '^[[Z' reverse-menu-complete
-    '';
-  };
-
-  programs.bash = {
-    inherit shellAliases;
     enable = true;
   };
 
---
+++ home-manager/includes/shell.nix
@@ -0,0 +1,83 @@
+{ pkgs, lib, ... }:
+
+with lib;
+
+let
+  shellAliases = let
+      cfg-src = "~/src/dotfiles/home-manager";
+      ehome-cmd = file: with pkgs; ''
+        git -C ${cfg-src} pull && \
+        nvim ${cfg-src}/${file} && \
+        git -C ${cfg-src}/${file} add ${cfg-src}/${file} && \
+        sudo nixos-rebuild switch --update-input monoid-home --update-input light-control && \
+        git -C ${cfg-src} commit
+      '';
+    in {
+      ls = "ls --color=auto";
+      l = "ls -alh";
+      ll = "ls -lh";
+      la = "ls -a";
+
+      # Ensure ssh'ed hosts understand kitty
+      ssh = "kitty +kitten ssh";
+
+      ehome = ehome-cmd "";
+      evim = ehome-cmd "includes/vim.nix";
+
+      enix = ''
+        sudo -E nvim /etc/nixos/ && \
+          sudo -E ${pkgs.git}/bin/git -C /etc/nixos add /etc/nixos && \
+          sudo /run/current-system/sw/bin/nixos-rebuild switch && \
+          sudo -E ${pkgs.git}/bin/git -C /etc/nixos commit
+      '';
+    };
+in {
+  programs.powerline-go = {
+    enable = true;
+    modules = [ "host" "ssh" "cwd" "nix-shell" "exit" ];
+    modulesRight = [ "git" "goenv" "venv" ];
+    settings = {
+      hostname-only-if-ssh = true;
+      cwd-max-depth = 4;
+      theme = "gruvbox";
+      numeric-exit-codes = true;
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    # Gruvbox dark
+    colors = {
+      "fg" = "#ebdbb2";
+      "bg" = "#282828";
+      "hl" = "#fabd2f";
+      "fg+" = "#ebdbb2";
+      "bg+" = "#3c3836";
+      "hl+" = "#fabd2f";
+
+      "info" = "#83a598";
+      "prompt" = "#bdae93";
+      "spinner" = "#fabd2f";
+      "pointer" = "#83a598";
+      "marker" = "#fe8019";
+      "header" = "#665c54";
+    };
+  };
+
+  programs.zsh = {
+    inherit shellAliases;
+    enable = true;
+    enableSyntaxHighlighting = true;
+
+    defaultKeymap = "viins";
+    initExtra = ''
+      # Enable shift-tab reverse completion
+      bindkey '^[[Z' reverse-menu-complete
+    '';
+  };
+
+  programs.bash = {
+    inherit shellAliases;
+    enable = true;
+  };
+}
